5cd503eea8bac24644738370ec0f313f8a1e0558
akanksha-v <akankshaverma282007@gmail.com>
2021-02-27 13:29:56 +0530

    updated firebase settings

--- firebase.py
+++ firebase.py
@@ -10,12 +10,12 @@ db = firebase.database()
 def login():
 
     email = input("Please Enter Your Email Address : \n")
-    password = getpass("Please Enter Your Password : \n")
+    password = getpass.getpass("Please Enter Your Password : \n")
     login = auth.sign_in_with_email_and_password(email, password)
     print("You are logged in!")
     lst1 = []
     botlst1 = [] 
-    useremail = email
+    useremail = email.split("@")[0]
     json_lst = db.child(useremail).child("lst").get().val()
     json_botlst = db.child(useremail).child("botlst").get().val()
     lst = list(json_lst.split(" ")) 
@@ -25,9 +25,9 @@ def login():
     json_lst1 = ' '.join([str(elem) for elem in lst1]) 
     json_botlst1 = ' '.join([str(elem) for elem in botlst1]) 
     
-    json_lst + = json_lst1
-    json_botlst + = json_lst1
-    db.child(useremail).child("lst").update(json_lst)          #can be used to update data 
+    json_lst += json_lst1
+    json_botlst += json_lst1
+    db.child(useremail).child("lst").update(json_lst)                        #can be used to update data 
     db.child(useremail).child("botlst").update(json_botlst)
 
     
@@ -36,15 +36,15 @@ def register():
     password = getpass.getpass("Please Choose a Password : \n")
     user = auth.create_user_with_email_and_password(email, password)
     print("User is created successfully!")
-    login = auth.sign_in_with_email_and_password(email, password) #logged in
-    useremail = email                                             #logged in user email
-    lst, botlst = registergame()                                  #function returns two lists
+    login = auth.sign_in_with_email_and_password(email, password)             #logged in
+    useremail = email.split("@")[0]                                           #logged in user email
+    lst, botlst = registergame()                                              #function returns two lists
     
     json_lst = ' '.join([str(elem) for elem in lst]) 
     json_botlst = ' '.join([str(elem) for elem in botlst]) 
     db.child(useremail).child("lst").set(json_lst)             
     db.child(useremail).child("botlst").set(json_botlst)
-
+    print("THANKS FOR PLAYING!")
 
 l_r = """
 Do you want to login or register?
